19b3c3c0b84b5688149e10025175785ad5a71af2
birakov <artembirakov@gmail.com>
2026-06-19 19:35:54 +0700

    .

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const sequelize = require('./config/db');
-
-// Импорт роутеров
-const tripsRouter = require('./routes/trips');
-const participantsRouter = require('./routes/participants');
+const cors = require('cors');
 
 dotenv.config();
 
+const sequelize = require('./config/db');
+const tripsRouter = require('./routes/trips');
+const participantsRouter = require('./routes/participants');
+const usersRouter = require('./routes/users');
+const userRolesRouter = require('./routes/userroles');
+
 const app = express();
+
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:3000', // ← Измени на адрес твоего фронтенда
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Логгер
+// Логирование запросов
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-app.use('/public', express.static(__dirname + '/public'));
-
-// Подключение маршрутов
+// Маршруты
 app.use('/api/trips', tripsRouter);
-app.use('/api/participants', participantsRouter);   // ← исправил путь
+app.use('/api/participants', participantsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/userroles', userRolesRouter);
 
 app.get('/', (req, res) => {
-  res.send('Добро пожаловать в Hiking Planner API');
+  res.send('Hiking Planner API is running');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 sequelize.sync({ alter: true })
   .then(() => {
--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
     logging: false,
   }
 );
